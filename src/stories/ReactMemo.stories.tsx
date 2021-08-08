bf62774b2977bc344a05c4d150ab162e166a3716
DK-AC <denkacaj@gmail.com>
2021-08-08 14:31:24 +0300

    fix immutability users

--- src/stories/ReactMemo.stories.tsx
+++ src/stories/ReactMemo.stories.tsx
@@ -11,7 +11,6 @@ export const NewMessagesCounter = (props: { count: number }) => {
 }
 
 export const UsersSecret = (props: { users: Array<string> }) => {
-    console.log('Users')
     return (
         <div>
             {props.users.map((u, i) => {
@@ -27,17 +26,19 @@ const Users = React.memo(UsersSecret)
 export const Example = () => {
 
     const [counter, setCounter] = useState(0)
-    const [users, setUsers] = useState(['Denis', 'Dima'])
+    const [users, setUsers] = useState(['Denis', 'Dima', 'Liudmila', 'Jenya'])
 
     const countHandler = () => setCounter(counter + 1)
+    const addUser = () => {
+        const newUser=[...users,'Sasha']
+        setUsers(newUser)
+    }
 
 
     return (
         <div>
-            <button
-                onClick={countHandler}>
-                +
-            </button>
+            <button onClick={countHandler}>+</button>
+            <button onClick={addUser}>addUser</button>
             <NewMessagesCounter count={counter}/>
             <Users users={users}/>
         </div>
